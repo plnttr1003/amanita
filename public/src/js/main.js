ae5ab450f3a23a9be65d1e1d518a5a1cb06f1cae
plnttr1003 <top@nrdn.ru>
2015-11-01 13:21:05 +0300

    design

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,19 +1,14 @@
 $(document).ready(function() {
-	function getRandom (min, max) {
-		var rand = min - 0.5 + Math.random()*(max-min)
-		return Math.round(rand);
-	}
-
-
+	var sho = $('.slider_image').length;
+	var i = 0;
+	$('.slider_image').eq(0).css({'z-index':'10', 'opacity': '1'});
 	function showOpacity() {
-		//console.log(w_count);
-		var count = $('.slider_image').length;
-		var w_count = getRandom(0, count);
-		var old_rand = w_count;
-		console.log(w_count);
-		$('.slider_image').css({'z-index':'1', 'opacity': '0'});
-		$('.slider_image').eq(w_count).css({'z-index':'10','opacity':'1'});
+		console.log('sho: '+ sho +', i: ' + i);
+		if (i !== sho)
+		{$('.slider_image').css({'z-index':'1', 'opacity': '0'}).eq(i).css({'z-index':'10','opacity':'1'}) }
+		else
+		{$('.slider_image').eq(0).css({'z-index':'10','opacity':'1'}); i=0}
+		i++;
 	}
-
 	setInterval(showOpacity,2000);
 });
